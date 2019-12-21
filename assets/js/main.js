f223cbd8c11137e08a62324c1c12b1e7cda55e2e
Neverlink <neverlink1337@gmail.com>
2019-12-21 22:39:28 +0200

    Update stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,6 @@ window.onload = () => {
     bgVideo.volume = 0.05;
     document.oncontextmenu = () => false;
 }
-function toggle1(el){
-  if (bgVideo.currentTime > 0) {
-    bgVideo.pause();
-  }
-}
 function toggle(el){
     if (el.className=="button fas fa-play")
     {
