43e3ef08afe0ac55d820b9ba072e7e0d48043d01
Neverlink <neverlink1337@gmail.com>
2020-02-24 02:41:40 +0200

    Add songs and randomizer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,45 @@
 window.onload = () => {
     document.getElementById("slider").defaultValue = 100;
-    audio.volume = document.getElementById("slider").value / 1000;
+    music.volume = document.getElementById("slider").value / 1000;
+
     document.oncontextmenu = () => false;
+
+    const songs = [
+        "media/alright (prod. by kid pretzel).mp3",
+        "media/knots(kir).mp3",
+        "media/familymart (Xangang).mp3",
+        "media/Facelift ft. dolly [prod. kiryano].mp3",
+        "media/3 checks (prod. maknae).mp3", 
+        "media/Paradise Kiss (Prod. mixed matches).mp3",
+        "media/kurtains x whitetrash - devil cry (prod. kurtains).mp3",
+        "media/maxwell (prod. pitfall + tempuraroll + lotchek).mp3",
+        "media/mbn.mp3",
+        "media/gone (prod. maknae).mp3",
+        "media/BANKAI (卍解) (ft. sholoh).mp3",
+    ];
+   
+    let playing = Math.floor(Math.random() * songs.length - 1);
+
+    document.getElementById("music").addEventListener('ended', (e) => {
+        playing = (playing + 1) % songs.length;
+        document.getElementById("played").src = songs[playing];
+        e.srcElement.load();
+        e.srcElement.play();
+    }, false);
 }
-function toggle(el){
+function toggle(el) {
     if (el.className=="button fas fa-play")
     {
-        audio.play();
+        music.play();
         el.className="button fas fa-pause";
     }
     else
     {
-        audio.pause();
+        music.pause();
         el.className="button fas fa-play";
     }
     return false;
 }
-window.changeVolume = function(val)
-{
-    audio.volume = val / 1000;
+window.changeVolume = function(val) {
+    music.volume = val / 1000;
 }
